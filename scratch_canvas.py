669852bc0e5f9311152885e00053517c4851452c
Khushikam <94278433+Khushikam@users.noreply.github.com>
2021-12-06 20:39:04 +0530

    Add files via upload

---
+++ scratch_canvas.py
@@ -0,0 +1,34 @@
+side = int(input('enter the value for side'))
+peri_sq = 4 * side
+area_sq = side ** 2
+area_cube = 6 * side ** 2
+vol_cube = side ** 3
+print('''
+The perimeter of square is : ''', peri_sq)
+print('''
+The area of square is : ''', area_sq)
+print('''
+The area of cube is : ''', area_cube)
+print('''
+The volume of cube is : ''', vol_cube)
+radius = int(input('enter the value for radius '))
+peri_cir = (2 * (22 / 7)) * radius
+area_cir = (22 / 7) * radius ** 2
+area_sph = (4 * (22 / 7)) * radius ** 2
+vol_sph = ((4 / 3) * (22 / 7)) * radius ** 3
+print('''
+The perimeter of circle is : ''', peri_cir)
+
+print('''
+The area of circle is : ''', peri_cir)
+
+print('''
+The area of sphere is : ''', peri_cir)
+
+print('''
+The volume of sphere is : ''', peri_cir)
+
+area_cone = ((22 / 7) * side) * radius
+
+print('''
+The area of cone is : ''', area_cone)
